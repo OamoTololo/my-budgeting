0bfff6e6dfe0e09555e84e5e894bea6c3b48121a
Oamogetswe Mgidi <oamotebogomgidi@gmail.com>
2025-11-13 10:22:50 +0200

    Created the application entry point by loading the autoloader, initializing the logger, created a log startup and routed the request.

--- public/index.php
+++ public/index.php
@@ -1,1 +1,13 @@
 <?php
+use App\Core\Logger;
+
+// Autoload all classes
+require __DIR__ . '/../vendor/autoload.php';
+
+// Initialize the logger
+$logger = new Logger();
+$logger->info("===  Application starting ===");
+
+echo "<h1>Welcome to My Budgeting App</h1>";
+
+$logger->info("Homepage displayed successfully.");
